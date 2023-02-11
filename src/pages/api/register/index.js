dbfeffe37306028c4b0b6095c76a6515769dc1c8
Norma <normamoras@gmail.com>
2023-02-11 16:01:29 -0500

    adding server function

--- src/pages/api/register/index.js
+++ src/pages/api/register/index.js
@@ -1,1 +1,14 @@
 const orm = require( '../../../../db/orm.mongoose' );
+
+// //Registration
+// post('/api/user/registration', async function( req,res ){
+//     const userData = req.body;
+//     const registerResult = await orm.registerUser( userData );
+//     res.send(registerResult);
+//   })
+
+ export default async function( req,res ){
+    const userData = req.body;
+    const registerResult = await orm.registerUser( userData );
+    res.send(registerResult);
+  }
