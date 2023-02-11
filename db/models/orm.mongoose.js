fa69bb9ef6ade0cc8f5337da638f826da531ca89
Norma <normamoras@gmail.com>
2023-02-11 15:28:49 -0500

    updating db connection

--- db/models/orm.mongoose.js
+++ db/models/orm.mongoose.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/bookforum",{useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser: true});
