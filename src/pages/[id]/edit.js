4f17ed4a93b63b52abd7265df5409b86154c5ac9
Norma <normamoras@gmail.com>
2023-03-01 17:44:46 -0500

    creating edit page

--- src/pages/[id]/edit.js
+++ src/pages/[id]/edit.js
@@ -1,6 +1,16 @@
+import React from 'react';
+
+import InputForm from '@/components/Forms/InputForm';
+
 const edit = () => {
-    return ( <>
-    </> );
+    return ( 
+        <InputForm 
+        onSubmit={handleForm}
+        handleChange={handleChange}
+        heading="Add a new User"
+        buttonText = "Update Info"
+      />
+     );
 }
  
 export default edit;
